e573ad1d9fe70d185b33f2e4f84c39b2dfb75838
Nikita Makhov <defite@gmail.com>
2019-11-07 20:57:12 +0300

    feat: add exclude option

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ module.exports = postcss.plugin('postcss-prepend-imports', function (opts) {
     opts = opts || {};
     opts.path = opts.path || '';
     opts.files = opts.files || [];
+    opts.exclude = opts.exclude || [];
 
+    const filePath = root.source.input.file;
+
+    if (filePath && opts.exclude.find(
+            (excludeItem) => filePath.includes(excludeItem)
+        )) {
+        return;
+    }
+    
     return function (root) {
         opts.files.reverse().forEach(file => {
             file = path.resolve(opts.path, file);
